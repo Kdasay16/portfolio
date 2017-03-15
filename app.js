99fe3bc94254e7724f9496c5c9a81c6b6a871d8d
Kayla Asay <kdasay16@gmail.com>
2017-03-15 12:07:28 -0700

    outline

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function Projects () {
+  for(var key in rawProjects) {
+    this[key] = rawProjects[key];
+  }
+};
+
+Projects.prototype.toHtml = function() {
+  var $newProject = $('article.outline').clone();
+  $newProject.removeClass('outline');
+};
+
+$newProject.find('h1').text(this.title);
+$newProject.find('h3').text(this.date);
+$newProject.find('.about').text(this.about);
+
+rawProjects.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(proj) {
+  $('.projects').append(proj.toHtml());
+});
---
+++ projData.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var rawProjects = [
+  {
+    title: 'Project One',
+    date: '00/00/2017',
+    about: '<p>This project was inspired by fun.</p>',
+    preview: 'img',
+  },
+  {
+    title: 'Project Two',
+    date: '00/00/2017',
+    about: '<p>Blah blah blah talking about stuff.</p>',
+    preview: 'img',
+  },
+  {
+    title: 'Project Three',
+    date: '00/00/2017',
+    about: '<p>More talking here</p>',
+    preview: 'img',
+  },
+];
